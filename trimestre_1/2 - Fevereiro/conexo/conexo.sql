c4f18936b718ca3d11adc2970de1b42e2c830ae2
Lucas Lara <lucas.060406@bento.local>
2024-02-23 10:04:30 -0300

    add: banco de dados de teste

--- trimestre_1/2 - Fevereiro/conexo/conexo.sql
+++ trimestre_1/2 - Fevereiro/conexo/conexo.sql
@@ -1,24 +1,21 @@
 create database conexo
 
-– Professores –
+-- Professores –
 
 create table professores (
 	id int not null primary key auto_increment,
 	nome varchar(255) not null
 )
 
-insert into professores (nome) values ('professor 1')
-insert into professores (nome) values ('professor 2')
-insert into professores (nome) values ('professor 3')
-insert into professores (nome) values ('professor 4')
-insert into professores (nome) values ('professor 5')
+insert into professores (nome) values ('professor 1'), ('professor 2'), ('professor 3'), ('professor 4'), ('professor 5')
 
-– Disciplinas –
+-- Disciplinas –
 create table disciplinas (
 id int not null primary key auto_increment,
 	nome varchar(255) not null,
-	professor_id int not null,
-	grupos_id int
+	professor_id int not null
+    --,
+	--grupos_id int
 	)
 
 ALTER TABLE disciplinas
@@ -27,22 +24,22 @@ ADD FOREIGN KEY (professor_id) REFERENCES professores(id)
 ALTER TABLE disciplinas
 ADD FOREIGN KEY (grupos_id) references grupos(id)
 
-insert into disciplinas (nome, professor_id) values ('disciplina 1', 1)
+insert into disciplinas (nome, professor_id) values ('disciplina 1', 1), ('disciplina 2', 2), ('disciplina 3', 3), ('disciplina 4', 4), ('disciplina 5', 5)
 
-– Grupos –
+-- Grupos –
 
 create table grupos (
 id int not null primary key auto_increment,
 	nome varchar(255) not null,
-disciplinas_id int not null
+--disciplinas_id int not null
 )
 
 ALTER TABLE grupos
 ADD FOREIGN KEY (disciplinas_id) references disciplinas(id)
 
-insert into grupos (nome, disciplinas_id ) values ('grupo 1', 1)
+insert into grupos (nome) values ('grupo 1'), ('grupo 2'), ('grupo 3'), ('grupo 4'), ('grupo 5')
 
-– grupos_disciplinas
+-- grupos_disciplinas
 
 create table grupos_disciplinas (
 	id int not null primary key auto_increment,
@@ -52,17 +49,20 @@ create table grupos_disciplinas (
 	foreign key (disciplina_id) references disciplinas(id)
 )
 
+insert into grupos_disciplinas (grupo_id, disciplina_id) values (1, 1), (1, 2), (1, 3), (1, 4), (1, 5)
+insert into grupos_disciplinas (grupo_id, disciplina_id) values (2, 1), (1, 2), (4, 3), (2, 4), (3, 5)
 
 
-– Palavras –
+-- Palavras –
 
 create table palavras (
 	id int not null primary key auto_increment,
 	palavra varchar(255) not null
 )
 
+insert into palavras (palavra) values ('palavra 1'), ('palavra 2'), ('gpalavra 3'), ('palavra 4'), ('palavra 5')
 
-– grupos_palavras–
+-- grupos_palavras–
 
 create table grupos_palavras (
 	id int not null primary key auto_increment,
@@ -71,3 +71,6 @@ create table grupos_palavras (
 	foreign key (grupo_id) references grupos(id),
 	foreign key (palavra_id) references palavras(id)
 )
+
+insert into grupos_palavras (grupo_id, palavra_id) values (1, 1), (1, 2), (1, 3), (1, 4), (1, 5)
+insert into grupos_palavras (grupo_id, palavra_id) values (2, 1), (1, 2), (4, 3), (2, 4), (3, 5)
